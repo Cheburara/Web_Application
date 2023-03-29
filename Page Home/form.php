41575afd2adf8f57fe0d8d149672290a553e4364
iltrei <iltrei@ttu.ee>
2023-03-29 21:14:09 +0000

    Update form.php

--- Page Home/form.php
+++ Page Home/form.php
@@ -48,7 +48,7 @@ if(isset($_POST['submit'])) {
             $_SESSION['loggedIn'] = true;
 
             // Redirect the user to the home page
-            header('Location: my_profile.html');
+            header('Location: my_profile.php');
             exit;
         }
     }
