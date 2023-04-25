6b7ad6fa7e393c4bad331e79607438b3ea423e8a
iltrei <iltrei@ttu.ee>
2023-04-25 10:46:07 +0000

    Update index.php

--- Page Home/index.php
+++ Page Home/index.php
@@ -22,6 +22,34 @@ session_start();
                 <h1>MAKE THE WORLD A CLEANER PLACE, <br>STARTING WITH YOUR OWN SPACE</h1>
             </div>
         </div>
+        <script>
+            const text = document.getElementById('text');
+            const strText = text.textContent;
+            const splitText = strText.split("");
+            text.textContent = "";
+
+            for(let i=0; i < splitText.length; i++){
+                text.innerHTML += "<span>" + splitText[i] + "</span>";
+            }
+
+            let char = 0;
+            let timer = setInterval(onTick, 50);
+
+            function onTick(){
+                const span = text.querySelectorAll('span')[char];
+                span.classList.add('fade');
+                char++;
+                if(char === splitText.length){
+                    complete();
+                    return;
+                }
+            }
+
+            function complete(){
+                clearInterval(timer);
+                timer = null;
+            }
+        </script>
         <footer>
             <div class="footer-content">
                 <h3>EcoClean</h3>
