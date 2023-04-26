f0415f4b89a86c602b1a788e014efd778b25eca4
arroba <arooba@ttu.ee>
2023-04-26 16:13:27 +0300

    index js

--- Page Home/index.php
+++ Page Home/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once('db-connection.php');
 session_start();
 ?>
@@ -7,21 +7,24 @@ session_start();
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="home.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"  /> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"  />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Questrial&family=Work+Sans&display=swap" rel="stylesheet">    
+        <link href="https://fonts.googleapis.com/css2?family=Questrial&family=Work+Sans&display=swap" rel="stylesheet">
         <title>Home: The Gargbage collection</title>
+
     </head>
     <body>
-        <?php 
-       require_once('header.php'); 
+        <?php
+       require_once('header.php');
         ?>
         <div class="container">
             <div class="overlay">
-                <h1>MAKE THE WORLD A CLEANER PLACE, <br>STARTING WITH YOUR OWN SPACE</h1>
+                <h1 id="text">MAKE THE WORLD A CLEANER PLACE, <br>STARTING WITH YOUR OWN SPACE</h1>
             </div>
         </div>
+
+
         <footer>
             <div class="footer-content">
                 <h3>EcoClean</h3>
@@ -37,5 +40,35 @@ session_start();
                 <p>Copyright</p>
             </div>
         </footer>
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                const text = document.getElementById('text');
+                const strText = text.textContent;
+                const splitText = strText.split("");
+                text.textContent = "";
+
+                for(let i=0; i < splitText.length; i++){
+                    text.innerHTML += "<span>" + splitText[i] + "</span>";
+                }
+
+                let char = 0;
+                let timer = setInterval(onTick, 50);
+
+                function onTick(){
+                    const span = text.querySelectorAll('span')[char];
+                    span.classList.add('fade');
+                    char++;
+                    if(char === splitText.length){
+                        complete();
+                        return;
+                    }
+                }
+
+                function complete(){
+                    clearInterval(timer);
+                    timer = null;
+                }
+            });
+        </script>
     </body>
 </html>
