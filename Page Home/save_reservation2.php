218885644de0980f25980e9de3c7fff347320976
iltrei <iltrei@ttu.ee>
2023-05-05 16:15:11 +0000

    Replace save_reservation2.php

--- Page Home/save_reservation2.php
+++ Page Home/save_reservation2.php
@@ -8,43 +8,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["submitReservation"]))
     $timehours = sanitize_input($_POST["timehour2"]);
     $services = sanitize_input($_POST["type"]);
     $address = sanitize_input($_POST["address"]);
-    require_once('db-connection.php');
-
-    session_start();
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["submitReservation"])) {
-        $timehours = sanitize_input($_POST["timehour2"]);
-        $time2 = $_POST["day"] ?? array();
-        $services = sanitize_input($_POST["type"]);
-        $address = sanitize_input($_POST["address"]);
-        $price = sanitize_input($_POST["garbage-price"]);
-
-        try {
-            $conn = new PDO("mysql:host=localhost;dbname=db_iltrei", 'iltrei', 'ilja2002');
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-            exit();
-        }
-
-        // Get the user ID from the users table
-        $stmt = $conn->prepare("SELECT id FROM users WHERE username = ?");
-        $stmt->execute([$_SESSION["username"]]);
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        $user_id = $user['id'];
-
-        // Insert the reservation into the reservations table
-        $stmt = $conn->prepare("INSERT INTO reservations (user_id, timehour, time2, service, address, price) VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$user_id, $timehours, $time2, $services, $address, $price]);
-
-        // After the reservation is successfully added to the database
-        $_SESSION['reservation_sent'] = true;
-        header('Location: services.php');
-        exit();
-    }
-
-
-    $price = sanitize_input($_POST["price"]);
+    $price = sanitize_input($_POST["garbage-price"]);
+    $days = sanitize_input($_POST["day"] ?? array());
 
     try {
         $conn = new PDO("mysql:host=localhost;dbname=db_iltrei", 'iltrei', 'ilja2002');
@@ -61,12 +26,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["submitReservation"]))
     $user_id = $user['id'];
 
     // Insert the reservation into the reservations table
-    $stmt = $conn->prepare("INSERT INTO reservations (user_id, timehour, service, address, price) VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->execute([$user_id, $timehours, $services, $address, $price]);
+    $stmt = $conn->prepare("INSERT INTO reservations (user_id, timehour, service, address, price, selected_days) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->execute([$user_id, $timehours, $services, $address, $price, $days]);
 
     // After the reservation is successfully added to the database
     $_SESSION['reservation_sent'] = true;
-    header('Location: index.php');
+    header('Location: services.php');
     exit();
 }
-
