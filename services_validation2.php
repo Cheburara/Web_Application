f97c94d67a89d36098e2fd5a831e3410f608769b
iltrei <iltrei@ttu.ee>
2023-05-17 11:52:20 +0000

    Update services_validation2.php

--- services_validation2.php
+++ services_validation2.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 
 function sanitize_input($input) {
     if (is_array($input)) {
@@ -16,50 +16,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["submitReservation"]))
     $timehours = sanitize_input($_POST["timehour2"]);
     $services = sanitize_input($_POST["type"]);
     $address = sanitize_input($_POST["address"]);
-    $days= sanitize_input(["day"] ?? array());
+    $days = sanitize_input($_POST["day"] ?? array());
     $price = sanitize_input($_POST["garbage-price"]);
-    $address = sanitize_input($_POST["address"]);
     $postalcode = sanitize_input($_POST["postcode"]);
 
     // Validation rules
     $isValid = true;
-    if(empty($address)) {
+    $errors = array();
+
+    if (empty($address)) {
         $isValid = false;
-        echo "Invalid address!";
+        $errors[] = "Invalid address!";
     } else if (!preg_match('/^[\p{L}0-9_ \-\',.;:]+(?:, [\p{L}0-9_ \-\',.;:]+){2,3}$/u', $address)) {
         $isValid = false;
-        echo "Invalid address!";
+        $errors[] = "Invalid address!";
     }
-    
-    
-    
-    if(empty($postalcode)) {
+
+    if (empty($postalcode)) {
         $isValid = false;
-        echo "Invalid code!";
+        $errors[] = "Invalid code!";
     } else if (!preg_match('/^[0-9]{1,8}$/', $postalcode)){
         $isValid = false;
-        echo "Invalid code!";
+        $errors[] = "Invalid code!";
     }
-    
-    
+
     $timehourss = array("9:00-12:00", "13:00-16:00", "17:00-20:00");
     if (empty($timehours) || !in_array($timehours, $timehourss)) {
         $isValid = false;
-        echo "Invalid time slot!<br>";
+        $errors[] = "Invalid time slot!";
     }
 
     $servicess = array("recyclable", "non-recyclable", "both");
     if (empty($services) || !in_array($services, $servicess)) {
         $isValid = false;
-        echo "Invalid service!<br>";
+        $errors[] = "Invalid service!";
     }
 
-    
     if (count(array($days)) > 2 || count(array($days)) == 0) {
         $isValid = false;
-        echo "Invalid number of days selected!<br>";
+        $errors[] = "Invalid number of days selected!";
     }
-
 
     if ($isValid) {
         // Validation successful
@@ -70,6 +66,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["submitReservation"]))
             'garbage-price' => $price
         );
         require_once "save_reservation2.php"; // Redirect to the next page
+        exit();
+    } else {
+        // Store error messages in session variable and redirect to errors.php
+        $_SESSION['errors'] = $errors;
+        header("Location: errors.php");
         exit();
     }
 }
